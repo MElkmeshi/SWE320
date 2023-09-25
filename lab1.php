5c95f5bcfecbedbbaaee9f91a2dbda03095bc34d
MElkmeshi <mohamed2108926@miuegypt.edu.eg>
2023-09-25 12:32:28 +0300

    add more questions

--- lab1.php
+++ lab1.php
@@ -4,47 +4,47 @@ function sortt($a){
 
     sort($a);
     foreach($a as $y){
-        echo $y . "<br>";
+        return $y . "<br>";
     }
 }
 $a = array(51,14,1,21);
-sortt($a);
+echo sortt($a);
 // Q2
 function IsPrime($number){
     if ($number == 1 || $number == 2)
-    echo "Prime<br>";
+    return "Prime<br>";
     for ($i = 2; $i <= $number/2; $i++){
         if ($number % $i == 0)
-        echo "Not Prime<br>";
+        return "Not Prime<br>";
     }
-    echo "Prime<br>";
+    return "Prime<br>";
 }
-IsPrime(7);
-IsPrime(9);
+echo IsPrime(7);
+echo IsPrime(9);
 
 // Q3
 function rev($a){
-    echo strrev(strval($a)) . "<br>";
+    return strrev(strval($a)) . "<br>";
 }
-rev(1234);
+echo rev(1234);
 
 // Q4
 function median($arr){
     $arr_freq = array_count_values($arr);    
     arsort($arr_freq);
     $new_arr = array_keys($arr_freq);
-    echo "Most frequent element is:"." ".$new_arr[0]. "<br>";
+    return "Most frequent element is:"." ".$new_arr[0]. "<br>";
 }
 $arr = array(1,2,3,4,5,5,5,5,5,6);
-median($arr);
+echo median($arr);
 
 // Q5
 function getlastlen($words){
     $word = explode(" ",$words);
     $newword = $word[count($word)-1];
-    echo strlen($newword) . "<br>";
+    return strlen($newword) . "<br>";
 }
-getlastlen("Hello World");
+echo getlastlen("Hello World");
 
 // Q6
 function isPower($x, $y)
@@ -56,33 +56,32 @@ function isPower($x, $y)
      }       
 	if($x == 1)
     {
-		echo $a. " is power of ". $b. "<br>";
+        return $a. " is power of ". $b. "<br>";
     }
     else
     {
-		echo $a." is not power of " .$b. "<br>";
+		return $a." is not power of " .$b. "<br>";
     }
   
 }
-isPower(16,2);
-isPower(12,2);
-isPower(81,3);
+echo isPower(16,2);
+echo isPower(12,2);
+echo isPower(81,3);
 
 // Q7
 function removecommas($str){
-    echo str_replace(array(",","."),"",$str) . "<br>";
+    return str_replace(array(",","."),"",$str) . "<br>";
 }
-removecommas("2,543.12");
+echo removecommas("2,543.12");
 
 // Q8
 function check($arr){
     for ($i = 0; $i < count($arr)-2; $i++){
         if ($arr[$i] == 1 && $arr[$i+1] == 2 && $arr[$i+2] == 3){
-            echo "True<br>";
-            return;
+            return "True<br>";
         }
     }
-    echo "False<br>";
+    return "False<br>";
 }
 $arr = array(1,2,3,4,5,6,7,8,9);
 echo check($arr);
@@ -91,15 +90,32 @@ echo check($arr);
 function checknum($arr,$num){
     for ($i = 0; $i < count($arr); $i++){
         if ($arr[$i] == $num){
-            echo "True<br>";
-            return;
+            return "True<br>";
         }
     }
-    echo "False<br>";
+    return "False<br>";
 }
 $arr = array(1,2,3,5,6,7,8,9);
 echo checknum($arr,4);
 
 // Q10
+function checknumfrom100($num1,$num2){
+    if($num1 == $num2 ){
+        return 0;
+    }
+    if($num1 > 100 || $num2 > 100){
+        return $num1 > $num2 ? $num1 : $num2;
+    }
+    $tmp1 = $num1;
+    $tmp2 = $num2;
+    while($tmp1 != 100 && $tmp2 != 100){
+        $tmp1++;
+        $tmp2++;
+    }
+    return $tmp1 == 100 ? $num1 : $num2;
+}
+echo checknumfrom100(78,92);
+
+//Q11
 
 ?>
